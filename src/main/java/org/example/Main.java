6baacc98133b0c97f5afb250c9711652001970ed
Brandon <brandon_smith3@icloud.com>
2025-04-15 20:19:52 -0700

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+public class Main {
+
+    static Map<String, String> env = System.getenv();
+    static String myAPIKEY = env.get("EBIRD_API_KEY");
+
+    public static void main(String[] args) throws IOException {
+
+        /*for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                    envName,
+                    env.get(envName));
+        }*/
+
+        Gson gson = new Gson();
+
     }
+
+
+
+        public static String getHotSpotInfo(String locationID) throws IOException{
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody body = RequestBody.create(mediaType, "");
+            Request request = new Request.Builder()
+                    .url("https://api.ebird.org/v2/ref/hotspot/info/L2619655")
+                    .method("GET",null) //cannot have body even though API doc says to
+                    .addHeader("X-eBirdApiToken", myAPIKEY)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                assert response.body() != null;
+
+
+                return response.body().string();
+
+            }
+
+    }
+
+
 }
